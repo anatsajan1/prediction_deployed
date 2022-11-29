c1b773f8d547a4756bd40964664754f594f4db62
anatsajan1 <anatsajan999@gmail.com>
2022-11-29 20:32:44 +0530

    first commit

--- Project/core/home/models.py
+++ Project/core/home/models.py
@@ -5,16 +5,16 @@ from django.db import models
 class Users(models.Model):
     username=models.CharField(max_length=100)
     email=models.EmailField(max_length=100)
-    phone=models.IntegerField(max_length=10)
+    phone=models.IntegerField()
     password=models.CharField(max_length=100)  
 
 class Application(models.Model):
     username=models.CharField(max_length=100)
     gender=models.CharField(max_length=32)
     email=models.EmailField(max_length=100)
-    income=models.IntegerField(max_length=15)
-    occupation=models.IntegerField(max_length=20)
-    category=models.IntegerField(max_length=20)
+    income=models.IntegerField()
+    occupation=models.IntegerField()
+    category=models.IntegerField()
     place=models.CharField(max_length=100)
     institute=models.CharField(max_length=100)
     source=models.CharField(max_length=100)
